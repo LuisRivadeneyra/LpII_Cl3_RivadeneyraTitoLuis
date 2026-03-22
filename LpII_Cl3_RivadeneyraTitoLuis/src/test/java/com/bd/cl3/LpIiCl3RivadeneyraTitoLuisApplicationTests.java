10e2eae3f8510de7acb8839291580be6053bff74
Luis Rivadeneyra <174075124+LuisRivadeneyra@users.noreply.github.com>
2026-03-22 12:01:03 -0500

    Update LpIiCl3RivadeneyraLuisApplicationTests.java

--- LpII_Cl3_RivadeneyraTitoLuis/src/test/java/com/bd/cl3/LpIiCl3RivadeneyraTitoLuisApplicationTests.java
+++ LpII_Cl3_RivadeneyraTitoLuis/src/test/java/com/bd/cl3/LpIiCl3RivadeneyraTitoLuisApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LpIiCl3RivadeneyraTitoLuisApplicationTests {
+class LpIiCl3RivadeneyraLuisApplicationTests {
 
 	@Test
 	void contextLoads() {
